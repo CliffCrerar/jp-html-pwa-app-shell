01f4c7afa4119c0c821ab6aaf4e2d2365201de32
CliffCrerar <cliff.crerar@gmail.com>
2020-05-06 03:42:15 +0200

    Building pwa api

--- server.js
+++ server.js
@@ -2,23 +2,36 @@
 
 // Variable Declarations
 const
+    date = new Date().toLocaleString().replace(/\//g, '-').replace(/:/g, '_'),
+    conf = require('./conf.json'),
     http = require('http'),
+    https = require('https'),
     url = require('url'),
     path = require('path'),
     fs = require('fs'),
     logDir = path.join(__dirname, 'logs'),
-    logFile = path.join(logDir, 'log-' + new Date()),
+    logFile = path.join(logDir, 'log-' + date),//.replace(/:/g,'-').replace('.','_'),
     publicDir = path.join(__dirname, 'public'),
     indexFile = fs.readFileSync(path.join(publicDir, 'index.html')),
+    errorPage = fs.readFileSync(path.join(publicDir, 'error.html')),
     msg = {
-        InitNotComplete: 'Incomplete Initialization, Cannot Start Server',
-    }
+        InitNotComplete: 'Incomplete Initialization, Cannot Start Server'
+    };
 // Fucntion expression declaration
+
 const
     createLogsDirectory = () => !fs.existsSync(logDir) && fs.mkdirSync(logDir),
-    createLog = () => fs.writeFileSync(logFile, 'LOG STARTED ' + new Date() + '\n\n', 'utf8');
+    createLog = () => {
+        try {
+            createLogsDirectory();
+            fs.writeFileSync(logFile, 'LOG STARTED ' + new Date() + '\n\n', 'utf8')
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
 // function statement declarations
+console.log('logFile: ', logFile);
 
 function environmentVariables() {
     try {
@@ -30,17 +43,19 @@ function environmentVariables() {
 }
 
 
-function requestLogger( { protocol, host, path, query } ) {
+function requestLogger({ protocol, host, path, query }) {
     try {
-        const msg = `${new Date()} ->PROTOCOL:${protocol},HOST:${host}, PATH:${path}, QUERY: ${query}\n`;
+        const msg = `${new Date().toLocaleString()} ->PROTOCOL:${protocol},HOST:${host}, PATH:${path}, QUERY: ${query}\n`;
         return fs.appendFileSync(logFile, msg, 'utf8');
     } catch (err) {
         throw new Error(err);
     }
 }
 
-function extensionToMimeType(){
-
+function errorLogger(err) {
+    const thisError = new Error(err);
+    const msg = `${new Date().toLocaleString()} ERROR: ${thisError}`;
+    return fs.appendFileSync(logFile, msg, 'utf8');
 }
 
 function deleteLogFiles() {
@@ -54,29 +69,30 @@ function deleteLogFiles() {
 }
 
 
-function getMime(ext, callback){
+function getMime(callback) {
 
-    s
-
-}
-
-
-function serveFile(publicFile,callback){
-    const
-        fileToGet = path.join(publicDir, publicFile === ('/' || '') ? 'index.html' : publicFile);
-        fs.existsSync(fileToGet) 
-            ? callback(true,fs.readFileSync(fileToGet), 'text/html')
-            : callback(false, null, null);
+    const req = https.request(conf.mimes, res => {
+        let payload = '';
+        res.on('data', data => payload += data);
+        res.on('end', () => callback(payload))
+    })
+    req.on('error', err => {
+        callback(err);
+        req.end;
+    })
+    req.end()
 }
 
 function Initialize() {
     return new Promise((resolve, reject) => {
         try {
             environmentVariables();
-            createLogsDirectory();
+            // createLogsDirectory();
             // deleteLogFiles();
             createLog();
-            return resolve();
+            getMime(mimes => {
+                return resolve(mimes);
+            })
         } catch (err) {
             return reject(err);
         }
@@ -85,25 +101,49 @@ function Initialize() {
 
 // Main execution block
 
-Initialize().then(() =>
+Initialize().then((mimes) => {
+    console.log('mimes: ', mimes)
 
     http.createServer(function (request, response) {
-        const urlObject = url.parse('http://' + req.headers.host + req.url)
-
+        let file = '';
         try {
+            const urlObject = url.parse('http://' + request.headers.host + request.url)
+            const fileRequestPath = path.join(publicDir, urlObject.path === '/' ? 'index.html' : urlObject.path);
+            const fileRequestPathDetials = path.parse(fileRequestPath);
+            file = fs.readFileSync(fileRequestPath);
+            console.log('path', urlObject.path);
+            console.log('urlObject: ', urlObject);
+            console.log('fileRequestPath: ', fileRequestPath);
+            console.log('fileRequestPathDetials: ', fileRequestPathDetials);
 
             requestLogger(urlObject);
-            response.writeHead(200, "OK", "text/html");
-            serveFile(urlObject.path)
+            let Mime = 'text/html'
+            // serveFile(urlObject.path,function(isPublicFile, File, Mime){
+            // if(isPublicFile){
+            response.writeHead(200, "OK", Mime);
 
         } catch (err) {
-            response.writeHead(500, 'Internal Server Error', 'application/json');
-            response.end(err);
+            file = errorPage;
+            errorLogger(err);
+            console.log('err: ', err);
+            console.log('err: ', err.code);
+            const thisError = new Error(err);
+
+            console.log('thisError: ', thisError);
+
+            switch (err.code) {
+                case 'ENOENT': response.writeHead(404, thisError.message, 'text/html'); break;
+                default: response.writeHead(500, thisError.message, 'text/html');
+            }
         }
+        response.end(file);
 
     }).listen(8080)
 
-).catch(err => {
+}).catch(err => {
+    console.error(err);
+    throw new Error(msg.InitNotComplete)
+}).catch(err => {
     console.error(err);
     throw new Error(msg.InitNotComplete)
 })
